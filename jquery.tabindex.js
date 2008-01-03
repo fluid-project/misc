04f148485a3f1ded4af3a9e4e7031ae3d3dac2f9
Colin Clark <colinbdclark@gmail.com>
2008-01-03 22:12:39 +0000

    FLUID-155: Simplified code based on Simon's suggestions. We no longer have a separate check for IE; everyone can use the .getAttributeNode ("tabIndex").specified check. Added a new normalizeTabIndexName () method.
    svn path=/sandbox/tabindex/trunk/; revision=4573

--- jquery.tabindex.js
+++ jquery.tabindex.js
@@ -1,42 +1,27 @@
 (function ($) {
 	// Private functions.
+	var normalizeTabIndexName = function () {
+	    return jQuery.browser.msie ? "tabIndex" : "tabindex";
+	}
+
 	var getValue = function (elements) {
 		if (!elements.hasTabIndex ()) {
 			return null;
 		}
 
 		// Use getAttribute because IE doesn't seem to like jQuery's .attr function.
+		var value = elements[0].getAttribute (normalizeTabIndexName ());
+
 		// Normalize the result to a number value for convenience.
-		return Number(elements[0].getAttribute ("tabIndex"));
+		return Number (value);
 	};
-	
+
 	var setValue = function (elements, toIndex) {
 		return elements.each (function () {
-			$ (this).attr ("tabIndex", toIndex);
+			$ (this).attr (normalizeTabIndexName (), toIndex);
 		});
 	};
-	
-	var tabIndexExistsIE = function (elements) {
-		// This will report wrong on old versions if IE that don't have the getAttributeNode function.
-		// As far as I know, there's simply no reliable way of determining if a tabindex exists in IE < 6.
-		if (!elements[0].getAttributeNode || !elements[0].getAttributeNode ("tabIndex").specified) {
-			return false;
-		}
-		
-		return true;
-	};
-	
-	var tabIndexExistsNonIE = function (elements) {
-		var tabIndexValue = elements.attr ("tabIndex");
-		// Be careful; some browsers will return number values for real tab indexes,
-		// so we can't just check if the tabindex value is falsey.
-		if (tabIndexValue === null || tabIndexValue === undefined) {
-			return false;
-		}
-		
-		return true;
-	};
-	
+
 	// Public functions.
 	$.fn.tabIndex = function (toIndex) {
 		if (toIndex !== null && toIndex !== undefined) {
@@ -45,17 +30,15 @@
 			return getValue (this);
 		}
 	};
-	
+
 	$.fn.removeTabIndex = function () {
 		return this.each(function () {
-			$ (this).removeAttr ("tabIndex");
+			$ (this).removeAttr (normalizeTabIndexName ());
 		});
 	};
-	
+
 	$.fn.hasTabIndex = function () {
-		if ($.browser.msie) {
-			return tabIndexExistsIE (this);
-		}
-		return tabIndexExistsNonIE (this);
+	    var attributeNode = this[0].getAttributeNode (normalizeTabIndexName ());
+        return attributeNode ? attributeNode.specified : false;
 	};
 }) (jQuery);
