3d3609d28ae2f6c820461a062527433acdd13e9d
Colin Clark <colinbdclark@gmail.com>
2008-02-08 03:07:22 +0000

    FLUID-154: Updated the checkbox example to use Eli's progressive enhancement suggestion (thanks Eli!) and to provide a more appealing visual appearance.
    svn path=/sandbox/tabindex/trunk/; revision=4682

--- examples/AccessibleCheckbox.js
+++ examples/AccessibleCheckbox.js
@@ -3,49 +3,57 @@ var fluid = fluid || {};
 fluid.accessiblecheckbox =  function () {
     // Private functions.
     var selectCheckbox = function (boxToFocus) {
-    	jQuery (boxToFocus).addClass ("focussed");
+        jQuery ("label", boxToFocus).addClass ("focussed");
     };
 
     var unselectCheckbox = function (boxToBlur) {
-    	jQuery (boxToBlur).removeClass ("focussed");
+        jQuery ("label", boxToBlur).removeClass ("focussed");
     };
 
     var leaveCheckboxes = function (selectedBox) {
-    	unselectCheckbox (selectedBox);
+        unselectCheckbox (selectedBox);
     };
 
     var toggleBox = function (box) {
-        var text = box.text();
-        if (text === "Yes") {
-            box.text("No");
-        } else if (text === "No") {
-            box.text("Yes");
-        }
+        box.toggleClass ("checked");
     };
 
     var selectCheckboxHandler = function (boxToCheck) {
-        var checkbox = jQuery (boxToCheck);
-        var childBoxIdSelector = "#" + checkbox.attr ("id") + "_box";
-        var box = jQuery (childBoxIdSelector, checkbox);
-        toggleBox (box);
+        var boxLabel = jQuery ("label", boxToCheck);
+        toggleBox (boxLabel);
+    };
+
+    var replaceInputsWithGraphics = function (boxes) {
+        var replaceFn = function (index, box) {
+           // Hide the original input box.
+           jQuery ("input", box).addClass("hiddenBox");
+
+           // Replace it with a graphic on the label.
+           jQuery ("label", box).addClass("graphicBox");
+        };
+
+        boxes.each (replaceFn);
     };
 
     // Public members.
-	return {
-	    initializeCheckboxes: function (checkboxContainerId) {
-	    	// Make the overall container tab-focussable.
-	    	var checkboxContainer = jQuery ("#" + checkboxContainerId);
-	    	checkboxContainer.tabbable(checkboxContainer);
+    return {
+        initializeCheckboxes: function (checkboxContainerId) {
+            // Make the overall container tab-focussable.
+            var checkboxContainer = jQuery ("#" + checkboxContainerId);
+            checkboxContainer.tabbable(checkboxContainer);
 
-	    	// Individual checkboxes should be selectable.
-	    	var boxes = checkboxContainer.children ("#checkboxes_0,#checkboxes_1,#checkboxes_2,#checkboxes_3");
-	    	var selectionHandlers = {
-	    		willSelect: selectCheckbox,
-	    		willUnselect: unselectCheckbox,
-	    		willLeaveContainer: leaveCheckboxes
-	    	};
-	        boxes.selectable (checkboxContainer, selectionHandlers);
-	        boxes.activatable (selectCheckboxHandler);
-	    }
-	}; // End public return.
+            // Find all checkboxes and make them fancy.
+            var boxes = checkboxContainer.children ("#checkboxes_0,#checkboxes_1,#checkboxes_2,#checkboxes_3");
+            replaceInputsWithGraphics (boxes);
+
+            // Make them key navigable and activatable.
+            var selectionHandlers = {
+                willSelect: selectCheckbox,
+                willUnselect: unselectCheckbox,
+                willLeaveContainer: leaveCheckboxes
+            };
+            boxes.selectable (checkboxContainer, selectionHandlers);
+            boxes.activatable (selectCheckboxHandler);
+        }
+    }; // End public return.
 }(); // End fluid.accessiblecheckbox namespace.
